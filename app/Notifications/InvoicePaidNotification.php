6a196f572dfe0b3dba68133aaa137a2054ceeab0
johnirevormasiziliwu <jrevormasi@gmail.com>
2023-06-10 00:03:57 +0800

    responsive

--- app/Notifications/InvoicePaidNotification.php
+++ app/Notifications/InvoicePaidNotification.php
@@ -44,7 +44,7 @@ class InvoicePaidNotification extends Notification implements ShouldQueue
                     ->line("Jumlah yang harus dibayarkan adalah **".$this->total."** seperti yang tertera pada faktur. Sesuai dengan persyaratan kontrak kami, pembayaran harus dibuat tepat waktu. Kami telah memberikan layanan kepada Anda dengan sepenuh hati dan kami berharap Anda juga dapat memenuhi kewajiban Anda dalam hal pembayaran. Kami sangat menghargai hubungan bisnis yang baik dengan Anda dan kami berharap dapat terus bekerja sama dengan Anda dalam jangka panjang.")
                     ->line("Saya meminta Anda untuk segera membayar faktur ini dalam waktu 10 hari. Jika ada masalah dengan faktur atau informasi tambahan yang dibutuhkan, silakan hubungi kami segera.")
                     ->line("Terima kasih atas perhatian Anda pada masalah ini. Saya berharap dapat menerima pembayaran dari Anda segera.")
-                    ->action('Download Invoice', route('download.invoice', ['invoice' => $this->invoice]))
+                    ->action('Download Invoice', route('download.invoice', $invoice))
                     ->line("Hormat Kami")
                     ->line('Yayasan Hasnur Center');
     }
